838df8369464cef52b2784b7e316cb8eb886f377
Kimberly <kimberly@Administrators-MacBook-Pro.local>
2022-03-25 14:54:09 -0400

    Add GA tag

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,24 @@ module.exports = {
     description: "How Do We Plan for an Uncertain Future?",
     author: "@MAPC",
   },
-  plugins: ["gatsby-plugin-emotion"],
+  plugins: [
+    "gatsby-plugin-emotion",
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [
+          "G-8EYCJYR1W7"
+        ],
+        gtagConfig: {
+          optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        pluginConfig: {
+          head: false,
+          respectDNT: true,
+        },
+      },
+    },
+  ],
 };
